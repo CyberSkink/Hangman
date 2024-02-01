5c0f817eb8c0c77e3f7ef9b40232be43c0a8bc56
Loganasaurus Rex <loganasaur@gmail.com>
2024-01-31 20:59:20 -0800

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("HANGMAN...");
+        Console.WriteLine("GUESS THE CORRECT WORD IN ORDER TO SPARE HUMANITY...");
+        // create an array of words for the player to guess...
+        string[] words = new string[] {"console", "program", "algorithm", "circut", "one", "zero", "variable"};
+    }
+}
