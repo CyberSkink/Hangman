eb11ab34ef92a8b1527538156041af7d47451bed
Loganasaurus Rex <loganasaur@gmail.com>
2024-01-31 21:06:33 -0800

    Added temporary array entry

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,6 +7,6 @@ class Program
         Console.WriteLine("HANGMAN...");
         Console.WriteLine("GUESS THE CORRECT WORD IN ORDER TO SPARE HUMANITY...");
         // create an array of words for the player to guess...
-        string[] words = new string[] {"console", "program", "algorithm", "circut", "one", "zero", "variable"};
+        string[] words = new string[] {"notpernimant","console", "program", "algorithm", "circut", "one", "zero", "variable"};
     }
 }
